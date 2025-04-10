e47cc1ce7b09fe54924b23558f19cdf0d743cf99
Tomas B <tomasbraun2008@gmail.com>
2025-04-10 17:47:13 -0300

    funciona

--- Assets/Scripts/ConversionDeMonedas.cs
+++ Assets/Scripts/ConversionDeMonedas.cs
@@ -4,48 +4,48 @@ using UnityEngine;
 
 public class ConversionDeMonedas : MonoBehaviour
 {
-
-    //1# Declaracion de variables
+    // 1# Declaración de variables
     public float montoAConvertir;
     public string codigoDeMoneda;
     private float dineroConvertido;
     private float cotizacionMonedaElegida;
 
-
     // Start is called before the first frame update
     void Start()
     {
-        //2# Ingreso y validacion de datos
-
-        if (montoAConvertir >= 1000) {
+        // 2# Ingreso y validación de datos
+        if (montoAConvertir < 1000f)
+        {
             Debug.Log("El monto debe ser mayor a $1000.");
             return;
         }
 
-        if (codigoDeMoneda == "D")  
+        if (codigoDeMoneda == "D")
+        {
             cotizacionMonedaElegida = 1134.5f;
         }
-
-        else if (codigoDeMoneda == "E") {
+        else if (codigoDeMoneda == "E")
+        {
             cotizacionMonedaElegida = 1397.16f;
         }
-    
-        else if (codigoDeMoneda == "R") {
+        else if (codigoDeMoneda == "R")
+        {
             cotizacionMonedaElegida = 211.1f;
         }
-        
-        else {
-            Debug.log("Por favor selecciona una moneda valida.");
+        else
+        {
+            Debug.Log("Por favor selecciona una moneda válida.");
+            return;
         }
 
-        
-    
-            //Debug.Log("Tus $" + montoAConvertir + " pesos, se convirtieron a $" + (montoAConvertir / 1350 + " Dolares");
+        dineroConvertido = montoAConvertir / cotizacionMonedaElegida;
 
+        Debug.Log("Tus $" + montoAConvertir + " pesos, se convirtieron a $" + dineroConvertido + " " + codigoDeMoneda);
+    }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
-        
+
     }
 }
